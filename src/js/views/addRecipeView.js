479fe99958e84dc5ce8cbd8718ade8b15999330f
ANKITA-web34 <anki333soul@gmail.com>
2021-07-22 13:41:36 +0530

    create addRecipeView file

---
+++ src/js/views/addRecipeView.js
@@ -0,0 +1,11 @@
+import View from './View.js';
+
+class AddRecipeView extends View {
+  _parentElement = document.querySelector('.upload');
+
+  _generaterMarkup() {
+    
+  }
+}
+
+export default new AddRecipeView();
